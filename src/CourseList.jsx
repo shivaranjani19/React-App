12d37748d19652c67287177e20a5f332529d6323
shivaranjani19 <sivaranjanik23ec@psnacet.edu.in>
2026-06-29 22:01:28 +0530

    Refactor CourseList component to improve error handling and return structure

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,30 +1,51 @@
 import coding from "../data/assets/coding.png";
 import Course from "./Course";
 import { useState, useEffect } from "react";
+
 function CourseList(){
+
     const [courses,setCourses] = useState(null);
+
     const [dummy,setDummy] = useState(true);
-    const[error,setError] = useState(null);
-    useEffect(()=>{        
+
+    const [error,setError] = useState(null);
+
+    useEffect(()=>{    
+
         fetch('http://localhost:3000/courses')
         .then(response => {
+            if(!response.ok){
+                throw Error("couldnt retrive data");
+            }
             console.log(response);
             return response.json()
         }).then(data => setCourses(data))
         .catch((error) => {
             console.log(error.message);
+            setError(error.message);
     })
     },[]);
+
+
     //const vfmCourses = courses.filter((course) => course.price <200);
+
     //courses.sort((x,y) => y.price - x.price);
+
+
     function handleDelete(id){
         console.log(id);
         const newCourse = courses.filter((course)=> course.id != id)
         setCourses(newCourse);
     }
     if(!courses){
-        return <></>
+        return (
+        <>
+        <p>{error}</p>
+        </>
+    )
     }
+
+    
    const coursesList = courses.map(
     (course) => 
     <Course key={course.id} name={course.name}
